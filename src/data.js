7fa58ea6dd5820deb23166a8de5c72890cc2d4bf
Wilson <wj.buena@gmail.com>
2021-11-05 10:48:49 +0800

    Hat data stored in inventoryData array

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const inventoryData = [
+    { 
+        name: 'Hat',
+        price: 23.99,
+        img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hello-world-hat.jpg'
+    }
+]
