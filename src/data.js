b81eabca48cac23aef34a80f86551f2d9e51af6b
Wilson <wj.buena@gmail.com>
2021-11-05 10:51:41 +0800

    New data in inventory data

--- src/data.js
+++ src/data.js
@@ -3,5 +3,17 @@ export const inventoryData = [
         name: 'Hat',
         price: 23.99,
         img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hello-world-hat.jpg'
+    },
+
+    {
+        name: 'Joggers',
+        price: 45.99,
+        img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/sweatpants.jpg'
+    },
+
+    {
+        name: 'T-shirt',
+        price: 17.99,
+        img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/java-tee.jpg' 
     }
 ]
