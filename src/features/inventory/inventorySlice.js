5e28e453f6e242d32c3ee06b616df5bc18fbc42f
Wilson <wj.buena@gmail.com>
2021-11-10 09:51:06 +0800

    Switch statement inside inventoryreducer function

--- src/features/inventory/inventorySlice.js
+++ src/features/inventory/inventorySlice.js
@@ -8,3 +8,13 @@ export const loadData = (data) => {
 };
 
 const initialInventory = [];
+
+export const inventoryReducer = (inventory = initialInventory, action) => {
+    switch(action.type) {
+        case 'inventory/loadData': {
+            return action.payload;
+        }
+        default:
+            return inventory;
+    }
+}
