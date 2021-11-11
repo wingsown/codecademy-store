b0963f0aba2666e4b97aecb86ebccfee2e6cac0c
Wilson <wj.buena@gmail.com>
2021-11-11 12:05:44 +0800

    Imported needed functions from different locations

--- src/features/inventory/Inventory.jsx
+++ src/features/inventory/Inventory.jsx
@@ -1,2 +1,5 @@
 import React, { useEffect } from 'react';
 import { calculatePrice, getCurrencySymbol } from '../../utilities';
+import { addItem } from '../cart/cartSlice';
+import { loadData } from './inventorySlice';
+
