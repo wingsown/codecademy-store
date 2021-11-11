6c61e733ae39919fa3443eb2359f1e502da96597
Wilson <wj.buena@gmail.com>
2021-11-11 12:15:03 +0800

    Inventory component functions

--- src/features/inventory/Inventory.jsx
+++ src/features/inventory/Inventory.jsx
@@ -3,3 +3,42 @@ import { calculatePrice, getCurrencySymbol } from '../../utilities';
 import { addItem } from '../cart/cartSlice';
 import { loadData } from './inventorySlice';
 
+export const Inventory = ({ inventory, currencyFilter, dispatch}) => {
+    const onMount = () => {
+        dispatch(loadData());
+    };
+
+    useEffect(onMount, [dispatch]);
+
+    const onClickHandler = (inventoryItem) => {
+        dispatch(addItem(inventoryItem));
+    };
+
+    if (inventory.length === 0) {
+        return <p> Sorry, no products are currently available...</p>;
+    }
+
+    return <ul>
+        {inventory.map()}
+    </ul>;
+
+    function createInventoryItem(inventoryItem) {
+        const { price, name, img } = inventoryItem;
+
+        const displayPrice = calculatePrice(price, currencyFilter);
+            return (
+                <li>
+                    <img/>
+                    <h3>{name}</h3>
+                    <h3>
+                        {getCurrencySymbol(currencyFilter)}
+                        {displayPrice.toFixed(2)}
+                        {currencyFilter}
+                    </h3>
+                    <button>
+                        Add to Cart
+                    </button>
+                </li>
+            )
+    }
+}
