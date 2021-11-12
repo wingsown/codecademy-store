2be9a765118660f2655c524461cb4ae827351542
Wilson <wj.buena@gmail.com>
2021-11-12 10:14:16 +0800

    set and clear search term functions

---
+++ src/features/searchTerm/searchTermSlice.js
@@ -0,0 +1,12 @@
+export const searchTerm = (term) => {
+    return {
+        type: 'searchTerm/setSearchTerm',
+        payload: term
+    }
+}
+
+export const clearSearchTerm = () => {
+    return {
+        type: 'searchTerm/clearSearchTerm',
+    }
+}
