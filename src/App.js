fc03a83152bba56469fbf4a5dafd9e205c662068
Wilson <wj.buena@gmail.com>
2021-11-12 20:59:44 +0800

    Removed Inventory and Cart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { CurrencyFilter } from './features/currencyFilter/CurrencyFilter';
-import { Inventory } from './features/inventory/Inventory';
+
 
 
 
