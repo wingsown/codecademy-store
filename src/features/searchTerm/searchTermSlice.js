7618823711d87c95fb1b60c2b30af6c419ef8dbf
Wilson <wj.buena@gmail.com>
2021-11-12 10:19:28 +0800

    Search term reducer

--- src/features/searchTerm/searchTermSlice.js
+++ src/features/searchTerm/searchTermSlice.js
@@ -1,3 +1,18 @@
+const initialState = '';
+export const searchTermReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'searchTerm/setSearchTerm': {
+            const newTerm = action.payload;
+            return newTerm;
+        }
+        case 'searchTerm/clearSearchTerm': {
+            return '';
+        }
+        default: 
+            return state;
+    }
+}
+
 export const searchTerm = (term) => {
     return {
         type: 'searchTerm/setSearchTerm',
