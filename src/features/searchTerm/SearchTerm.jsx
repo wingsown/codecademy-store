dc42feb6a2bae52a3e46d37d34fe09c222a380ca
Wilson <wj.buena@gmail.com>
2021-11-12 10:34:12 +0800

    Event handler in search term component

---
+++ src/features/searchTerm/SearchTerm.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { setSearchTerm, clearSearchTerm } from './searchTermSlice';
+
+const searchIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/search.svg';
+const clearIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/clear.svg';
+
+export const SearchTerm = (props) => {
+    const { searchTerm, dispatch } = props;
+}
