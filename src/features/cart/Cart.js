350f48c7459798fb16ded8b9666c6aad2bc335ca
Wilson <wj.buena@gmail.com>
2021-12-02 10:56:38 +0800

    Cart component done

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { calculateTotal, getCurrencySymbol } from '../../utilities';
+import { calculatePrice, calculateTotal, getCurrencySymbol } from '../../utilities';
 import { changeItemQuantity } from './cartSlice';
 
 export const Cart = (props) => {
@@ -14,5 +14,53 @@ export const Cart = (props) => {
 
         //Otherwise, convert the input into a number and pass it along as the newQuantity
         const newQuantity = Number(input);
+    };
+
+    const cartElements = [];
+
+    for (let item in cart){
+        cartElements.push(createCartItem(item));
     }
+
+    const total = calculatePrice(cart, currencyFilter);
+
+    return (
+        <div id="cart-container">
+            <ul id="cart-items">{cartElements}</ul>
+            <h3 className="total">
+                Total{' '}
+                <span className="total-value">
+                    {getCurrencySymbol(currencyFilter)} {total} {currencyFilter}
+                </span>
+            </h3>
+        </div>
+    );
+
+    function createCartItem(itemName) {
+        const item = cart[itemName];
+
+        if (item.quantity === 0) {
+            return;
+        }
+
+        return (
+            <li key={itemName}>
+                <p>{itemName}</p>
+                <select
+                className="item-quantity"
+                value={item.quantity}
+                onChange={(e) => {
+                    onInputChangeHandler(itemName, e.target.value)
+                    changeItemQuantity(itemName, newQuantity);
+                }}>
+                    {[...Array(100).keys()].map((_,index) => (
+                        <option key={index} value={index}>
+                            {index}
+                        </option>
+                    ))}
+                </select>
+            </li>
+        )
+    }
+
 }
