a1a8936ea4b01afe00db6b0c15a958f865bf9ada
Wilson <wj.buena@gmail.com>
2021-12-02 09:44:09 +0800

    Modified props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { CurrencyFilter } from './features/currencyFilter/CurrencyFilter';
+import { Inventory } from './features/inventory/Inventory';
 
 
 export const App = (props) => {
@@ -9,7 +10,15 @@ export const App = (props) => {
 
   return (
     <div className="App">
-      <CurrencyFilter/>
+      <CurrencyFilter
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+      <Inventory
+        inventory={state.inventory}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
 
     </div>
   );
--- src/features/inventory/Inventory.js
+++ src/features/inventory/Inventory.js
@@ -1,46 +1,43 @@
 import React, { useEffect } from 'react';
-import { calculatePrice, getCurrencySymbol } from '../../utilities';
+
+import { calculatePrice, getCurrencySymbol} from '../../utilities';
 import { addItem } from '../cart/cartSlice';
 import { loadData } from './inventorySlice';
 
-export const Inventory = ({ inventory, currencyFilter, dispatch}) => {
-    const onMount = () => {
-        dispatch(loadData());
-    };
+export const Inventory = ({ inventory, currencyFilter, dispatch }) => {
+  const onMount = () => {
+    dispatch(loadData());
+  };
+  useEffect(onMount, [dispatch]);
 
-    useEffect(onMount, [dispatch]);
+  const onClickHandler = (inventoryItem) => {
+    dispatch(addItem(inventoryItem));
+  };
 
-    const onClickHandler = (inventoryItem) => {
-        dispatch(addItem(inventoryItem));
-    };
+  if (inventory.length === 0) {
+    return <p> Sorry, no products are currently available... </p>;
+  }
 
-    if (inventory.length === 0) {
-        return <p> Sorry, no products are currently available...</p>;
-    }
+  return <ul id="inventory-container">{inventory.map(createInventoryItem)}</ul>;
 
-    return <ul id="inventory-container">
-        {inventory.map(createInventoryItem)}
-    </ul>;
-
-    function createInventoryItem(inventoryItem) {
-        const { price, name, img } = inventoryItem;
-
-        const displayPrice = calculatePrice(price, currencyFilter);
-            return (
-                <li key={name} className="item">
-                    <img src={img} alt={''}/>
-                    <h3>{name}</h3>
-                    <h3 className="price">
-                        {getCurrencySymbol(currencyFilter)}
-                        {displayPrice.toFixed(2)}
-                        {currencyFilter}
-                    </h3>
-                    <button
-                    onClick={() => onClickHandler(inventoryItem)
-                    }>
-                        Add to Cart
-                    </button>
-                </li>
-            );
-    }
-}
+  function createInventoryItem(inventoryItem) {
+    const { price, name, img } = inventoryItem;
+    const displayPrice = calculatePrice(price, currencyFilter);
+    return (
+      <li key={name} className="item">
+        <img src={img} alt={''} />
+        <h3>{name}</h3>
+        <h3 className="price">
+          {getCurrencySymbol(currencyFilter)}
+          {displayPrice.toFixed(2)} {currencyFilter}
+        </h3>
+        <button
+          onClick={() => onClickHandler(inventoryItem)}
+          className="add-to-cart-button"
+        >
+          Add to Cart
+        </button>
+      </li>
+    );
+  }
+};
