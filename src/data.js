f424d96be3ac9c849caed343dbf6efce72285823
Wilson <wj.buena@gmail.com>
2021-12-13 11:06:50 +0800

    Edited inventory images

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ export const inventoryData = [
         itemName: 'Hat',
         price: 23.99,
         img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hello-world-hat.jpg'
+        // 
     },
 
     {
